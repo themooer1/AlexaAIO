a8a7f35a7aaa5ccf02cfbed78b116ad41c7a3f93
Eugene <fluffy1781@gmail.com>
2018-01-09 09:46:22 -0500

    Version 1.0a
    Things might work?

--- AIOspeechlet.py
+++ AIOspeechlet.py
@@ -108,29 +108,56 @@ def handle_session_end_request():
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
 
+def handlePlayByNumberIntent(intent, session):
+
+    card_title="playByNumberFailed"
+    session_attributes=session['attributes']
+    should_end_session = True
+
+    if 'EpisodeNumber' in intent['slots']:
+        episodeNumber = intent['slots']['EpisodeNumber']['value']
+        session_attributes = session['attributes']
+        if session_attributes['Radio']==True:
+            session_attributes.update({'lastPlayed':AIO.getRadioEpisodeByNumber(episodeNumber)})
+            session_attributes['Radio']=False
+            e=AIO.getRadioEpisodeByNumber(episodeNumber)
+        else:
+            session_attributes.update({'lastPlayed':AIO.getFreeEpisodeByNumber(episodeNumber)})
+            session_attributes['Free']=False
+            e=AIO.getFreeEpisodeByNumber(episodeNumber)
+        return build_response(session_attributes,start_play_url_response(e['url']))
+    else:
+        speech_output = "playByName was called, but no episode name was found. " \
+                        "Please try again."
+        reprompt_text = None
+        return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
 
 
 def handlePlayByNameIntent(intent, session):
     global default_session
     """Plays an Episode by it's name."""
 
-    card_title = ""
-    session_attributes = default_session
-    should_end_session = False
+    card_title = "playByNameFailed"
+    session_attributes = session['attributes']
+    should_end_session = True
 
     if 'EpisodeName' in intent['slots']:
         episodeName = intent['slots']['EpisodeName']['value']
         session_attributes = session['attributes']+{'lastPlayed':episodeName}
-        e=AIO.getFreeEpisodeByName(episodeName)
+        if session_attributes['Radio']==True:
+            e=AIO.getRadioEpisodeByName(episodeName)
+            session_attributes['Radio']=False
+        else:
+            e=AIO.getFreeEpisodeByName(episodeName)
+            session_attributes['Free']=False
+
         return build_response(session_attributes,start_play_url_response(e['url']))
 
     else:
-        speech_output = "I'm not sure what your favorite color is. " \
+        speech_output = "playByName was called, but no episode name was found. " \
                         "Please try again."
-        reprompt_text = "I'm not sure what your favorite color is. " \
-                        "You can tell me your favorite color by saying, " \
-                        "my favorite color is red."
-    return build_response(session_attributes, build_speechlet_response(
+        reprompt_text = None
+        return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
 def handleListRadioIntent():
@@ -144,27 +171,8 @@ def handleListFreeIntent():
     global default_session
     session_attributes = default_session
     session_attributes['Free']=True
-    speech_output = "I found some free episodes.  Interrupt me when you hear one you want to play.  " + ".  ".join(["Episode {0}.  ".format(ep['Number'])+ep['Name'] for ep in AIO.getFreeEpisodes()])
+    speech_output = "I found some free episodes.  Interrupt me when you hear one you want to play, and say the name of the episode.    " + ".  ".join(["Episode {0}.  ".format(ep['Number'])+ep['Name'] for ep in AIO.getFreeEpisodes()])
 
-def get_color_from_session(intent, session):
-    session_attributes = {}
-    reprompt_text = None
-
-    if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
-        favorite_color = session['attributes']['favoriteColor']
-        speech_output = "Your favorite color is " + favorite_color + \
-                        ". Goodbye."
-        should_end_session = True
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "You can say, my favorite color is red."
-        should_end_session = False
-
-    # Setting reprompt_text to None signifies that we do not want to reprompt
-    # the user. If the user does not respond or says something that is not
-    # understood, the session will end.
-    return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output, reprompt_text, should_end_session))
 
 
 # --------------- Events ------------------
